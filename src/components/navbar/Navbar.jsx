d8c85fd922ab2968e0362287f39a5f3cccda4c37
Ali <ghaddar.al@gmail.com>
2024-07-08 01:18:46 +0200

    Add project files

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,57 +1,60 @@
-import React from 'react'
+import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import LanguageOutlinedIcon  from "@mui/icons-material/LanguageOutlined";
-import DarkModeOutlinedIcon  from "@mui/icons-material/DarkModeOutlined";
-import FullscreenExitOutlinedIcon  from "@mui/icons-material/FullscreenExitOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
-import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
 
-import pexelsImage from "../../images/pexels.jpg"; // Correctly import the image
-
-
-import "./navbar.scss"
 const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="wrapper">
         <div className="search">
           <input type="text" placeholder="Search..." />
-          <SearchOutlinedIcon/>
+          <SearchOutlinedIcon />
         </div>
         <div className="items">
           <div className="item">
-          <LanguageOutlinedIcon className="icon"/>
-          English
+            <LanguageOutlinedIcon className="icon" />
+            English
           </div>
           <div className="item">
-          <DarkModeOutlinedIcon className="icon"/>
+            <DarkModeOutlinedIcon
+              className="icon"
+              onClick={() => dispatch({ type: "TOGGLE" })}
+            />
           </div>
           <div className="item">
-          <FullscreenExitOutlinedIcon className="icon"/>
+            <FullscreenExitOutlinedIcon className="icon" />
           </div>
           <div className="item">
-          <NotificationsNoneOutlinedIcon className="icon"/>
-          <div className="counter">1</div>
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
           </div>
           <div className="item">
-          <ChatBubbleOutlineOutlinedIcon className="icon"/>
-          <div className="counter">2</div>
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">2</div>
           </div>
           <div className="item">
-          <ListOutlinedIcon className="icon"/>
+            <ListOutlinedIcon className="icon" />
           </div>
           <div className="item">
-          <img
-              src={pexelsImage} // for the image
-              alt="Avatar"
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
               className="avatar"
             />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
