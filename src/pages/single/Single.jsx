a3e17e39420c9e1997e09a7b64f937775e6cb74c
Ali <ghaddar.al@gmail.com>
2024-07-08 01:16:29 +0200

    first commit

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,59 @@
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
+
+const Single = () => {
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/16657418/418252_766876.jpeg"
+                alt="Hassan Profile"
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Hassan Hammoud</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">Hassan.hd2003@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+961 79141457</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    Beirut,Hadath
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Lebanon</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+          </div>
+        </div>
+        <div className="bottom">
+        <h1 className="title">Last Transactions</h1>
+          <List/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Single;
